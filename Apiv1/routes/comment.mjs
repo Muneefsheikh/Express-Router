6d48ad973b5a6cbd2677272533bc130e7fb90fc1
Muneefsheikh <121711860+Muneefsheikh@users.noreply.github.com>
2023-07-21 02:31:56 +0500

    Add files via upload

---
+++ Apiv1/routes/comment.mjs
@@ -0,0 +1,20 @@
+import express from 'express';
+let router = express.Router()
+router.get('/comment/:postId/:commentId', (req, res, next) => {
+    console.log('this is signup!', new Date());
+    res.send('post created');
+})
+router.get('/comments/:postId', (req, res, next) => {
+    console.log('this is signup!', new Date());
+    res.send('post created');
+})
+router.put('/comment/:postId/:commentId', (req, res, next) => {
+    console.log('this is signup!', new Date());
+    res.send('post created');
+})
+router.delete('/comment/:postId/:commentId', (req, res, next) => {
+    console.log('this is signup!', new Date());
+    res.send('post created');
+})
+
+export default router
