47142f68cd3319a282d0ccc7b416699742610990
Muneefsheikh <121711860+Muneefsheikh@users.noreply.github.com>
2023-07-21 16:17:51 +0500

    Update

--- Apiv1/index.mjs
+++ Apiv1/index.mjs
@@ -8,33 +8,15 @@ import feedRouter from './routes/feed.mjs'
 import postRouter from './routes/post.mjs'
 
 
-// /api/v1/login
+
 router.use(authRouter)
 
 
-router.use((req, res, next) => {
-    if (token === "valid") {
-        next();
-    } else {
-        res.send({ message: "invalid token" })
-    }
-})
 
 router.use("/api/v1", commentRouter)
 router.use("/api/v1", postRouter)
 router.use("/api/v1", feedRouter)
 
 
-router.post("/weather", (req, res, next) => {
-
-    console.log("req.body: ", req.body);
-
-    res.send({
-        message: "weather is normal",
-        apiVersion: "v1",
-        temp: 32,
-        min: 20,
-    });
-})
 
 export default router
