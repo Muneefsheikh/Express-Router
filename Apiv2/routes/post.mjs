46d9c4137205ebeb0fa73e7e8f796cb5d5ccc1e6
Muneefsheikh <121711860+Muneefsheikh@users.noreply.github.com>
2023-07-21 16:11:50 +0500

    Update post.mjs

--- Apiv2/routes/post.mjs
+++ Apiv2/routes/post.mjs
@@ -5,21 +5,18 @@ let router = express.Router()
 
 
 
-// GET     /api/v1/post/:userId/:postId
-router.get('/post/:userId/:postId', (req, res, next) => {
-    console.log('this is signup!', new Date());
-    res.send('post created');
-})
-// GET     /api/v1/posts/:userId
 router.get('/posts/:userId', (req, res, next) => {
     console.log('this is signup!', new Date());
-    res.send('post created');
+    res.send('post v2');
 })
-// POST    /api/v1/post
+
+
 router.post('/post', (req, res, next) => {
     console.log('this is signup!', new Date());
-    res.send('post created');
+    res.send('post v2');
 })
+
+
 // PUT     /api/v1/post/:userId/:postId
 router.put('/post/:userId/:postId', (req, res, next) => {
     console.log('this is signup!', new Date());
@@ -28,7 +25,7 @@ router.put('/post/:userId/:postId', (req, res, next) => {
 // DELETE  /api/v1/post/:userId/:postId
 router.delete('/post/:userId/:postId', (req, res, next) => {
     console.log('this is signup!', new Date());
-    res.send('post created');
+    res.send('post deleted');
 })
 
 export default router
