d3cdfa66ef577fdace025f7bd7a2382093770439
Muneefsheikh <121711860+Muneefsheikh@users.noreply.github.com>
2023-07-21 16:07:14 +0500

    Update

--- Apiv2/routes/feed.mjs
+++ Apiv2/routes/feed.mjs
@@ -7,7 +7,7 @@ let router = express.Router()
 // GET     /api/feed/:userId
 router.get('/feed/:userId', (req, res, next) => {
     console.log('this is signup!', new Date());
-    res.send('post created');
+    res.send('feed v2');
 })
 
 
