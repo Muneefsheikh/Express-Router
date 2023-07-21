2086cb8ade52633c0f4d73e33efde97d027ef6ac
Muneefsheikh <121711860+Muneefsheikh@users.noreply.github.com>
2023-07-21 16:24:21 +0500

    Update

--- Apiv1/routes/comment.mjs
+++ Apiv1/routes/comment.mjs
@@ -1,20 +1,25 @@
 import express from 'express';
 let router = express.Router()
+
+
 router.get('/comment/:postId/:commentId', (req, res, next) => {
     console.log('this is signup!', new Date());
-    res.send('post created');
+    res.send('get comment v1');
 })
+
 router.get('/comments/:postId', (req, res, next) => {
     console.log('this is signup!', new Date());
-    res.send('post created');
+    res.send('get comments v1');
 })
+
 router.put('/comment/:postId/:commentId', (req, res, next) => {
     console.log('this is signup!', new Date());
-    res.send('post created');
+    res.send('put comment v1');
 })
+
 router.delete('/comment/:postId/:commentId', (req, res, next) => {
     console.log('this is signup!', new Date());
-    res.send('post created');
+    res.send('delete comment v1');
 })
 
 export default router
